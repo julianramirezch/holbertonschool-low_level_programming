38d7d2bebe9bb384d9adc49d26acd3d0eafe3152
julianramirezch <julianramirezch1@gmail.com>
2020-02-10 20:39:29 +0000

    1 exercise

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{ 
+  char j[10]="Holberton\n";
+  int u;
+  for(u=0;u<=10;u++)
+    {
+      _putchar(j[u]);
+    }
+ 
+return (0);
+} 
