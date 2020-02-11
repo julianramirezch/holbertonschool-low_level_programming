550ae37656131d1461d056ebb618d133d0bd8bca
julianramirezch <julianramirezch1@gmail.com>
2020-02-11 14:48:32 +0000

    Fix Style

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,11 +7,13 @@
  */
 void print_alphabet(void)
 {
-char j;
-for (j = 97 ; j <= 122 ; j++)
-{
-_putchar(j);
-}
-_putchar('\n');
-}
+	char j;
 
+	for (j = 97 ; j <= 122 ; j++)
+	{
+		_putchar(j);
+
+	}
+
+	_putchar('\n');
+}
