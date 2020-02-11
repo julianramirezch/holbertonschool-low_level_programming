9bca4d2ea1e342e8c58bab89583a0fe16bb7e20a
julianramirezch <julianramirezch1@gmail.com>
2020-02-11 14:58:05 +0000

    Fix function description

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 #include <stdio.h>
+
 /**
- * main - check the code for Holberton School students.
- *
+ * print_alphabet - Alphabet in lowercase
  * Return: Always 0.
  */
 void print_alphabet(void)
