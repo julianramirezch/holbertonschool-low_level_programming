66a78e17b46e2dbeef0a2e840ce42110b7c8bf34
julianramirezch <julianramirezch1@gmail.com>
2020-02-11 03:16:39 +0000

    function that prints the alphabet, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+char j;
+for (j = 97 ; j <= 122 ; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
+
