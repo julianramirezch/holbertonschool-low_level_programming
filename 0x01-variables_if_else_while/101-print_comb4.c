a2bb6a26bf595d5da1255ecd750ac4e3426df857
julianramirezch <julianramirezch1@gmail.com>
2020-02-16 21:38:39 +0000

    fix style

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,30 +8,28 @@
 
 int main(void)
 {
-	int j = 0;
-	int u , l , i , a;
+int j = 0;
+int u, l, i, a;
 
-	while ( j >= 0 && j <= 999)
-	{
-		u= j / 10;
-		l= u / 10 ;
-		i = u % 10;
-		a= j % 10;
-
-		if( (l != i &&  i != a && l !=a ) && ((l - i) < 0 && (i - a ) < 0))
-		{
-
-			putchar(l + 48);
-			putchar(i + 48);
-			putchar(a + 48);
-			if(j != 789)
-			{
-				putchar(44);
-				putchar(32);
-			}
-		}
-		j++;
-	}
-	putchar('\n');
-	return (0);
+while (j >= 0 && j <= 999)
+{
+	u = j / 10;
+	l = u / 10;
+	i = u % 10;
+	a = j % 10;
+if ((l != i &&  i != a && l != a) && ((l - i) < 0 && (i - a) < 0))
+{
+	putchar(l + 48);
+	putchar(i + 48);
+	putchar(a + 48);
+if (j != 789)
+{
+	putchar(44);
+	putchar(32);
+}
+}
+j++;
+}
+putchar('\n');
+return (0);
 }
