f80171fe025fbc8234f360466375cfa56411bc78
julianramirezch <julianramirezch1@gmail.com>
2020-02-16 15:06:20 +0000

    Fix Style

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
 
@@ -13,6 +13,7 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+
 if (n % 10  == 0)
 {
 	printf("Las digit of %i is %i and is 0\n", n, n % 10);
