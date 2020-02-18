922c02ca7f32248f4db697fd6173bd2f1d3fd0d8
julianramirezch <julianramirezch1@gmail.com>
2020-02-18 23:11:07 +0000

    strings$ git commit -m

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * puts_half - function that prints half o a string.
+ * @str: Char
+ * Return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+int j;
+int u;
+
+for (j = 0 ; *(str + j) != '\0' ; j++)
+{
+}
+for (u = (j / 2) ; u < j  ; u++)
+{
+	_putchar(*(str + u));
+}
+_putchar('\n');
+}
