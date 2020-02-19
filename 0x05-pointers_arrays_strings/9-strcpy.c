0781e92f182392e0ad383645383e274ad15534eb
julianramirezch <julianramirezch1@gmail.com>
2020-02-19 01:41:15 +0000

    function that copies the string pointed to by src ind dest

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy - function that copies the string pointed to by src,including '\0' in dest.
+ * @dest: Char.
+ * @src: char.
+ * Return: Always 0.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+char j;
+int u;
+int l;
+for (u = 0 ; *(src + u) != '\0' ; u++)
+{
+}
+for (l = 0 ; l <= u ; l++)
+{
+	j = *(src + l);
+	*(dest + l) = j;
+}
+return(dest);
+}
