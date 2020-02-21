663388bf304f386dd4ad9186e0c879fa139facd0
julianramirezch <julianramirezch1@gmail.com>
2020-02-21 18:53:22 +0000

    Encode a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,19 @@
+char *leet(char *s)
+{
+int j;
+int u;
+char a[] = "AaEeOoTtLl";
+char b[] = "4433007711";			    
+
+for (j = 0 ; *(s + j) != '\0' ; j++)
+{
+	for (u= 0 ; *(a + u) != '\0' ; u++)
+	{
+		if((*(s + j) == *(a + u)))
+		{
+			*(s + j) = *(b + u);
+		}
+	}
+}
+return(s);
+}
