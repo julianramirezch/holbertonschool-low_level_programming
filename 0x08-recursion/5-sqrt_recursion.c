32ad2b728c7c055bdf7fa53f3c6fe1696a0c1ecf
julianramirezch <julianramirezch1@gmail.com>
2020-02-26 21:05:24 +0000

    Function that returns root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+/**
+ * _sqrt_recursion - return the root
+ * @n: integer
+ * Return: integer
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (operation(n, 1));
+}
+
+/**
+ * root - Operations and validations.
+ * @n: integer
+ * @m: integer
+ * Return: Integer.
+ */
+
+int root(int n, int m)
+{
+
+if (n == 0)
+{
+	return (1);
+}
+if (n < 0)
+{
+	return (-1);
+}
+if (m * m == n)
+{
+	return (m);
+}
+else if (m * m > n)
+{
+	return (-1);
+}
+else
+{
+	return (root(n, m + 1));
+}
+}
