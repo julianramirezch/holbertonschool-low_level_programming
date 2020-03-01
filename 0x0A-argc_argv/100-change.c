f0a0d1035652bcca4bcf0f205841c030e6ae2ef2
julianramirezch <julianramirezch1@gmail.com>
2020-03-01 20:38:10 +0000

    Fix condition if change < 0

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -15,29 +15,30 @@ int change, coins;
 change = atoi(argv[1]);
 coins = 0;
 
-while (change > 0)
-{
-if (argc != 2)
-{
-	printf("Error\n");
-	return (1);
-}
-else if (change >= 25)
-	change = change - 25;
-else if (change >= 10)
-	change = change - 10;
-else if (change >= 5)
-	change = change - 5;
-else if (change >= 2)
-	change = change - 2;
-else 
-	change = change - 1;
-coins += 1;
-}
 if (change < 0)
-{
 	printf("0\n");
+else
+{
+	while (change > 0)
+	{
+		if (argc != 2)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else if (change >= 25)
+			change = change - 25;
+		else if (change >= 10)
+			change = change - 10;
+		else if (change >= 5)
+			change = change - 5;
+		else if (change >= 2)
+			change = change - 2;
+		else
+			change = change - 1;
+		coins += 1;
+	}
+	printf("%i\n", coins);
 }
-printf("%i\n", coins);
 return (0);
 }
