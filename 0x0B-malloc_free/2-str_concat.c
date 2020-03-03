aec6382371e459287befaa2c73001712c572eead
julianramirezch <julianramirezch1@gmail.com>
2020-03-03 20:33:39 +0000

    Add 2 if condition

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,7 +18,8 @@ char *str_concat(char *s1, char *s2)
 	int cont4;
 	char *dest;
 
-
+	if(!(s1 == NULL || s2 == NULL))
+	{
 	for (conts1 = 0 ; s1[conts1] ; conts1++)
 	{}
 	for (conts2 = 0; s2[conts2]; conts2++)
@@ -28,8 +29,8 @@ char *str_concat(char *s1, char *s2)
 	length++;
 
 	dest = malloc(sizeof(char) * length);
-	if(dest == NULL)
-		return(NULL);
+	if (dest == NULL)
+		return (NULL);
 	for (cont3 = 0; cont3 < conts1 && s1[cont3] ; cont3++)
 	{
 		dest[cont3] = s1[cont3];
@@ -39,4 +40,7 @@ char *str_concat(char *s1, char *s2)
 		dest[cont3] = s2[cont4];
 	}
 	return (dest);
+	}
+	else
+		return (NULL);
 }
