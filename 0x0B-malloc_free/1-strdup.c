1852ff1474975cf98a38475f1d15244fa4981217
julianramirezch <julianramirezch1@gmail.com>
2020-03-03 18:54:16 +0000

    Fix code return(str2)

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,4 +1,3 @@
-#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -15,7 +14,7 @@ int cont;
 int cont2;
 char *str2;
 
-for (cont = 0; str[cont] != '\0'; cont++)
+for (cont = 0; str[cont]; cont++)
 {}
 cont++;
 str2 = malloc(sizeof(char) * cont);
@@ -25,5 +24,5 @@ for (cont2 = 0 ; cont2 < cont ; cont2++)
 {
 	str2[cont2] = str[cont2];
 }
-return (str);
+return (str2);
 }
