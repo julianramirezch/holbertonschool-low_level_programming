d0947c43438952b4d01bf32249b4f986a4aa34f8
julianramirezch <julianramirezch1@gmail.com>
2020-03-05 20:58:52 +0000

    Fix size

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,7 +16,8 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	size = max - min + 1;
+	size = max - min ;
+	size++;
 	range = malloc(size * sizeof(int));
 	if (range == NULL)
 		return (NULL);
@@ -25,8 +26,6 @@ int *array_range(int min, int max)
 		range[cont1] = min;
 		min++;
 	}
-	cont1--;
-	range[cont1] = cont1;
 	return (range);
 
 }
