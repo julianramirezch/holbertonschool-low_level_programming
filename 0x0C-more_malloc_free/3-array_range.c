8b6572e6962fd6d43af4a7a0ad9f4afd15ff9a0f
julianramirezch <julianramirezch1@gmail.com>
2020-03-05 21:00:23 +0000

    Fix style

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,7 +16,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	size = max - min ;
+	size = max - min;
 	size++;
 	range = malloc(size * sizeof(int));
 	if (range == NULL)
