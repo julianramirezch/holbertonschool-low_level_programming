277b01fbde56d24d64cf9607871a4b0d32a0693f
julianramirezch <julianramirezch1@gmail.com>
2020-03-12 01:39:04 +0000

    program that performs simple operations.

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+
+/**
+ * main - Entry point
+ * @argc: size
+ * @argv: arguments
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int (*operator)(int, int);
+	int num1, num2;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	operator = get_op_func(argv[2]);
+	if (!operator)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	if ((argv[2] == || argv[2] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", operator(num1,  num2);
+	return (0);
+}
