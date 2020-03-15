156c1145aa199daec4fb9f9cb40d909c7cea86a6
julianramirezch <julianramirezch1@gmail.com>
2020-03-15 02:04:45 +0000

    Add prototypes

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -20,5 +20,9 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
+int _putchar(char c);
+void p_ch(va_list all);
+void p_int(va_list all);
+void p_float(va_list all);
+void p_string(va_list all);
 #endif
