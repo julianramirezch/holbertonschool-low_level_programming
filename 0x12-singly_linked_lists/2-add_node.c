d5e5f4079ccbf2403b4febfb2ebdff8c4a4796a5
julianramirezch <julianramirezch1@gmail.com>
2020-03-20 16:59:00 +0000

    [add]: function that adds a new node at the beginning of a list_t list.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,1 +1,27 @@
 #include "lists.h"
+
+/**
+ * add_node - function that adds a new node at the beginning of a list_t list.
+ * @head: Pointer to head.
+ * @str: String.
+ * Return: Head.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	int len;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+	
+	for(len = 0; str[len]; len++)
+	{};
+	new->str = strdup(str);
+	new->len = len;
+	new->next = *head;
+	*head = new;
+
+	return (*head);
+}
