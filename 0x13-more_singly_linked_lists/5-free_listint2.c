9bdd9764352f8bccab41d7d56de314686298a278
julianramirezch <julianramirezch1@gmail.com>
2020-03-23 20:06:55 +0000

    [add]: Write a function that frees a listint_t list.

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a listint_t list.
+ * @head: Double pointer.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *aux;
+
+	for (aux = *head; aux; aux = *head)
+	{
+		(*head) = (*head)->next;
+		free(aux);
+	}
+}
