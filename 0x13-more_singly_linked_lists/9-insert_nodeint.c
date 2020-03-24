85c03beb7623660d85c2f3f5ddca04f5ee0efddb
julianramirezch <julianramirezch1@gmail.com>
2020-03-24 23:42:29 +0000

    [fix]: fix code

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,26 +1,6 @@
 #include "lists.h"
 
 /**
- * add_nodeint - adds a new node at the beginning of a listint_t list.
- * @head: Pointer to head.
- * @n: Pointer to n
- * Return: Value of head
- */
-
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t *new;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = *head;
-	*head = new;
-	return (new);
-}
-
-/**
  * insert_nodeint_at_index - inserts a new node at a given position.
  * @head: Double pointer.
  * @idx: index
@@ -35,31 +15,33 @@ unsigned int cont, cont2;
 
 if (*head == NULL)
 	return (NULL);
+newn = malloc(sizeof(listint_t));
+if (newn == NULL)
+	return (NULL);
+newn->n = n;
+newn->next = NULL;
 aux = *head;
 if (idx == 0)
-	add_nodeint(head, n);
+{
+	newn->next = *head;
+	*head = newn;
+}
 if (idx > 0)
 {
 	for (cont = 0; aux; cont++)
 	{
 		if (cont == idx)
 		{
-			newn = malloc(sizeof(listint_t));
-			if (newn == NULL)
-				return (NULL);
-			newn->n = n;
 			newn->next = aux;
+			tmp = *head;
+			for (cont2 = 0; tmp; cont2++)
+			{
+				if (cont2 == (idx - 1))
+					tmp->next = newn;
+				tmp = tmp->next;
+			}
 		}
 		aux = aux->next;
-	}
-	tmp = *head;
-	for (cont2 = 0; tmp; cont2++)
-	{
-		if (cont2 == (idx - 1))
-		{
-			tmp->next = newn;
-		}
-		tmp = tmp->next;
 	}
 }
 return (newn);
