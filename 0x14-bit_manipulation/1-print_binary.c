151f436c5ded39da9ced086eab07cada825b6daa
julianramirezch <julianramirezch1@gmail.com>
2020-03-26 18:05:56 -0500

    [add]:  function that prints the binary representation of a number.

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * print_binary -  function that prints the binary representation of a number.
+ * @n: Unsigned long int
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int base = 0;
+	long int cont = 0, cont2, bin;
+
+	while (base <= n)
+	{
+		if (cont == 0)
+			base = 1;
+		else
+			base *= 2;
+		if (base > n)
+			break;
+		cont++;
+	}
+
+	if (n == 0)
+		_putchar(48);
+
+	for (cont2 = 0; cont > 0; cont2++)
+	{
+		cont--;
+		bin = n >> cont;
+		if (bin & 1)
+			_putchar(49);
+		else
+			_putchar(48);
+	}
+}
