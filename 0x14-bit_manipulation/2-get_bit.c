77b52f84a35b8e5d08e610886662f011ed312da3
julianramirezch <julianramirezch1@gmail.com>
2020-03-26 20:04:46 -0500

    [fix]: Betty style and comments

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,15 +1,17 @@
 #include "holberton.h"
 
 /**
- * print_binary -  function that prints the binary representation of a number.
+ * get_bit - Write a function that returns the value of a bit at a given index.
  * @n: Unsigned long int
+ * @index: index
+ * Return: 1 or 0
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int bits;
 	unsigned long int size;
-	
+
 	bits = n >> index;
 	size = sizeof(n) * 8;
 	if (index > size)
