4f66e855a86bcc10ce71b5ab9872d2d17439854c
julianramirezch <julianramirezch1@gmail.com>
2020-03-31 16:20:55 -0500

    [fix]: Empty string

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
-		text_content = '\0';
+		text_content = "";
 	for (len = 0; text_content[len]; len++)
 	{}
 
