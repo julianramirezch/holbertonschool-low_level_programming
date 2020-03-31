48a0e6b2ffadd938ffb6b475e6585f3646441e62
julianramirezch <julianramirezch1@gmail.com>
2020-03-31 15:40:08 -0500

    [fix]: Change data type writep to int

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	for (len = 0; text_content[len]; len++)
 	{}
 
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd == -1)
 		return (-1);
 	writep = write(fd, text_content, len);
