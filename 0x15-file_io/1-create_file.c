cb92a1dbe75853982fca221e95855c9d09ba74a0
julianramirezch <julianramirezch1@gmail.com>
2020-03-31 16:22:46 -0500

    [fix]: empty string

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,12 +11,12 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	int len;
-	int writep;
+	ssize_t writep;
 
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
-		text_content = '\0';
+		text_content = "";
 	for (len = 0; text_content[len]; len++)
 	{}
 
