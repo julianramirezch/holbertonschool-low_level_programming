74483aa9e805abb8423483a5c3707ab255e9a763
julianramirezch <julianramirezch1@gmail.com>
2020-03-31 23:57:28 -0500

    [fix]: Fix code to original variables

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,4 @@
 #include  "holberton.h"
-#include <stdio.h>
 
 /**
  * main -  program that copies the content of a file to another file.
@@ -10,23 +9,21 @@
 
 int main(int ac, char **av)
 {
-	int ff, ft, readp = 1024, writep, closeff, closeft;
+	int ff, ft;
 	char buffer[1024];
+	ssize_t writep, readp = 1, closeff, closeft;
 
 	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	ff = open(av[1], O_RDONLY);
 	if (ff == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	ft = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (ft == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
-	while (readp == 1024)
+	while (readp)
 	{
-		readp = read(ff, buffer, 1024);
+		readp = read(ff, buffer, sizeof(buffer));
 		if (readp == -1)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 		writep = write(ft, buffer, readp);
@@ -34,9 +31,9 @@ int main(int ac, char **av)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	}
 	closeff = close(ff);
-	closeft = close(ft);
 	if (closeff == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", ff), exit(100);
+	closeft = close(ft);
 	if (closeft == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", ft), exit(100);
 	return (0);
