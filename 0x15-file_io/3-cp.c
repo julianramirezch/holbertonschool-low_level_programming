ce8b5f2ebe72ea636d844a6a89a0f441a718fb10
julianramirezch <julianramirezch1@gmail.com>
2020-03-31 20:24:58 -0500

    [add]: program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,46 +11,39 @@ int main(int argc, char **argv)
 {
 	int ff, ft;
 	char buffer[1024];
-	ssize_t readp, writep;
+	ssize_t readp = 1, writep, closeff, closeft;
 
 	if (argc < 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	ff = open(argv[1], O_RDONLY);
 	if (ff == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
-		exit(98);
-	}
-	readp = read(ff, buffer, sizeof(buffer));
-	if (readp == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]), exit(98);
 	ft = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (ft == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
-		exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]), exit(99);
 	}
-	writep = write(ft, buffer, readp);
-	if (writep == -1)
+	while (readp)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
-		exit(99);
+		readp = read(ff, buffer, sizeof(buffer));
+		if (readp == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]), exit(98);
+		}
+		else
+		{
+			writep = write(ft, buffer, readp);
+			if (writep == -1)
+			{
+				dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]), exit(99);
+			}
+		}
 	}
-	if (close(ff) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", ff);
-		exit(100);
-	}
-	if (close(ft) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", ft);
-		exit(100);
-	}
+	closeff = close(ff);
+	closeft = close(ft);
+	if (closeff == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", ff), exit(100);
+	if (closeft == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", ft), exit(100);
 	return (0);
 }
