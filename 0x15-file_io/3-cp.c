2dc17a824ed6a1924a44c996ae31fc46be7359cb
julianramirezch <julianramirezch1@gmail.com>
2020-03-31 21:31:26 -0500

    [change]: Function name parameters

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,29 +7,29 @@
  * Return: 0 on success
  */
 
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
 	int ff, ft, readp = 1024, writep, closeff, closeft;
 	char buffer[1024];
 
-	if (argc != 3)
+	if (ac != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	ff = open(argv[1], O_RDONLY);
+	ff = open(av[1], O_RDONLY);
 	if (ff == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]), exit(98);
-	ft = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", av[1]), exit(98);
+	ft = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (ft == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s", av[2]), exit(99);
 	while (readp == 1024)
 	{
 		readp = read(ff, buffer, 1024);
 		if (readp == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]), exit(98);
+			dprintf(STDERR_FILENO, "Error: Can't read from %s\n", av[1]), exit(98);
 		if (readp > 0)
 		{
 			writep = write(ft, buffer, readp);
 			if (writep == -1)
-				dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]), exit(99);
+				dprintf(STDERR_FILENO, "Error: Can't write to %s", av[2]), exit(99);
 		}
 	}
 	closeff = close(ff);
