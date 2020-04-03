bd0bc4775584a273176a49d1accf59220108bbf0
julianramirezch <julianramirezch1@gmail.com>
2020-04-03 16:56:07 -0500

    2[add]:  function that locates a character in a string.

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _strchr - function that locates a character in a string.
+ * @s: String
+ * @c: byte
+ * Return: pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+	if (c == '\0')
+		return (s);
+	while (s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+}
