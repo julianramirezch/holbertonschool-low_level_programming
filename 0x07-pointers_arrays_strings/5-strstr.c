705cf2de33441552aa0bb9dba0e6825b0c188e39
julianramirezch <julianramirezch1@gmail.com>
2020-04-03 18:33:21 -0500

    [add]: function that locates a substring.

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ * _strstr - function that searches a string for any of a set of bytes.
+ * @haystack: String
+ * @needle: string
+ * Return: pointer
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int cont, cont2 = 0;
+	int *pos, index = 0;
+	int num = 0, len;
+
+	pos = &index;
+	for (len = 0; needle[len]; len++)
+	{};
+	for (cont = 0; haystack[cont]; cont++)
+	{
+		if (haystack[cont] == needle[cont2])
+		{
+			*pos += cont;
+			for (; needle[cont2]; index++, cont2++)
+			{
+				if (haystack[index] == needle[cont2])
+				num += 1;
+				else
+				{
+					cont2 = 0, *pos = 0, num = 0;
+					break;
+				}
+			}
+		}
+		if (num == len)
+		return (haystack + cont);
+	}
+	return (haystack + cont);
+}
