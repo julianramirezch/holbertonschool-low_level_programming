47e026b0fe0932473ee49a6bdb12e3c02f556d77
julianramirezch <julianramirezch1@gmail.com>
2020-05-02 14:54:10 -0500

    [fix]: add prev and next

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,15 +9,26 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *newn;
+	dlistint_t *newn, *aux;
 
 	newn = malloc(sizeof(dlistint_t));
 	if (newn == NULL)
 		return (free(newn), NULL);
 	newn->n = n;
 	newn->prev = NULL;
-	newn->next = *head;
+	newn->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = newn;
+		(*head)->next = NULL;
+		return (*head);
+	}
+
+	aux = *head;
+	newn->next = aux;
+	aux->prev = newn;
 	*head = newn;
 
-	return (*head);
+	return (newn);
 }
