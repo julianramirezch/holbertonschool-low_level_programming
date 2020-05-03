03d43eefe6b632471b51e75adc2112d0655608ed
julianramirezch <julianramirezch1@gmail.com>
2020-05-03 11:59:39 -0500

    [fix betty style]

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -32,7 +32,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			aux->prev = newn;
 			return (newn);
 		}
-		if (aux->next ==NULL && (cont + 1) == idx)
+		if (aux->next == NULL && (cont + 1) == idx)
 		{
 			free(newn);
 			return (add_dnodeint_end(h, n));
