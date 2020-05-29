b246f8e97f8bb7aa29333e0c6321d4ba3d7a8fac
julianramirezch <julianramirezch1@gmail.com>
2020-05-28 23:39:39 -0500

    [add]: Function delete ht

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - function that deletes a hash table.
+ * @ht: hash table
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	int index = 0, size = ht->size;
+	hash_node_t *aux, *head;
+
+	for (; index < size; index++)
+	{
+		if (ht->array[index] != NULL)
+		{
+			head = ht->array[index];
+			for (aux = head; aux; aux = head)
+			{
+				head = head->next;
+				free(aux->key);
+				free(aux->value);
+				free(aux);
+			}
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
