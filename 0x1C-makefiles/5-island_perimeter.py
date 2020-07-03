400f3745e87e0cf75a49bbca0f2c7e562cd3dd6d
julianramirezch <julianramirezch1@gmail.com>
2020-07-03 15:14:50 -0500

    [add]: perimeter of the island described in grid

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Module: Island Perimeter """
+
+
+def island_perimeter(grid):
+    """ returns the perimeter of the island described in grid """
+    y = 0
+    x = 0
+    for i in grid:
+        if i.count(1):
+            y += 1
+            if x < i.count(1):
+                x = i.count(1)
+    return (2 * x) + (2 * y)
