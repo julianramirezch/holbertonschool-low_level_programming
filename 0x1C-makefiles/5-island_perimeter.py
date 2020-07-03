277933c6e75cdb169761fbad0f8be3f806c66110
julianramirezch <julianramirezch1@gmail.com>
2020-07-03 15:16:08 -0500

    [fix]: Condition

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,9 +6,10 @@ def island_perimeter(grid):
     """ returns the perimeter of the island described in grid """
     y = 0
     x = 0
-    for i in grid:
-        if i.count(1):
-            y += 1
-            if x < i.count(1):
-                x = i.count(1)
+    if grid:
+        for i in grid:
+            if i.count(1):
+                y += 1
+                if x < i.count(1):
+                    x = i.count(1)
     return (2 * x) + (2 * y)
