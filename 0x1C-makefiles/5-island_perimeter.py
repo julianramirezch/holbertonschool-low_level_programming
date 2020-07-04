98dd36ba5505f9d10061c89432f78884e5e8d06e
julianramirezch <julianramirezch1@gmail.com>
2020-07-03 20:40:34 -0500

    [refactor]

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,7 +12,7 @@ def island_perimeter(grid):
                     perimeter += 1
                 if row > 0 and grid[row - 1][column] != 1:
                     perimeter += 1
-                if column < row and grid[row][column + 1] != 1:
+                if column < len(grid[row]) - 1 and grid[row][column + 1] != 1:
                     perimeter += 1
                 if grid[row][column - 1] != 1:
                     perimeter += 1
