2ff8fd7cedf4419d6ab4337576d473be050ce42a
julianramirezch <julianramirezch1@gmail.com>
2020-08-26 16:53:23 -0500

    [update]: Add prototypes to 1-binary.c work

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,5 +5,9 @@
 #include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int *shell_sort(int *array, size_t size);
+void print_array(const int *array, int l, int r);
+
 
 #endif
